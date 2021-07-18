bb1d24a683bf472a04b9be8eb98181194b83197b
Jeremy Kaplan <jdkaplan@metagram.net>
2021-07-17 23:08:07 -0700

    Fix Webpack dev build

--- webpack.common.js
+++ webpack.common.js
@@ -3,20 +3,18 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
-const crate = path.resolve(__dirname, ".");
-const dist = path.resolve(__dirname, "dist");
 module.exports = {
   entry: "./index.js",
   output: {
     filename: "index.js",
-    path: dist,
+    path: path.resolve(__dirname, "dist"),
   },
   plugins: [
     new HtmlWebpackPlugin({
       title: "Trellis",
     }),
     new WasmPackPlugin({
-      crateDirectory: crate,
+      crateDirectory: path.resolve(__dirname, "."),
     }),
     new MiniCssExtractPlugin(),
   ],
